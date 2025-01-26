6c88612e0a36380bbc97214ab9870f86ea5a59d5
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-01-26 23:18:40 +0530

    complete the ui of placeorder part

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = ({ children }) => {
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(true);
   const [cartItems, setCartItems] = useState({});
+  const navigate = useNavigate();
 
   const addToCart = async (itemId, size) => {
     if (!size) {
@@ -88,6 +90,7 @@ const ShopContextProvider = ({ children }) => {
     getCartCount,
     updateQuantity,
     getCartAmount,
+    navigate,
   };
 
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -3,10 +3,17 @@ import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
 import CartTotal from "../components/CartTotal";
+// import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  const { products, currency, cartItems, updateQuantity, getCartAmount } =
-    useContext(ShopContext);
+  const {
+    products,
+    currency,
+    cartItems,
+    updateQuantity,
+    getCartAmount,
+    navigate,
+  } = useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
 
   getCartAmount();
@@ -93,10 +100,18 @@ const Cart = () => {
             );
           })}
         </div>
-      </div>
-      <div className="flex justify-end my-20">
-        <div className="w-full sm:w-[450px]">
-          <CartTotal />
+        <div className="flex justify-end my-20">
+          <div className="w-full sm:w-[450px]">
+            <CartTotal />
+            <div className="w-full text-end">
+              <button
+                onClick={() => navigate("/place-order")}
+                className="bg-black text-white text-sm my-8 px-8 py-3"
+              >
+                Proceed to checkout
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
