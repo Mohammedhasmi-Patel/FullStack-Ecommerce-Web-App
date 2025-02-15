f2a5b681e122c2e6707a65449a8a52a162d5b872
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-02-16 04:13:00 +0530

    create product controller and productRoute

---
+++ server/controllers/productController.js
@@ -0,0 +1,17 @@
+// add product logic
+
+const addProduct = async () => {};
+
+// list prodcuts
+
+const listProduct = async () => {};
+
+// remove product
+
+const removeProduct = async () => {};
+
+// info for single product
+
+const singleProduct = async () => {};
+
+export { addProduct, listProduct, removeProduct, singleProduct };
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import conn from "./config/db.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoute.js";
 
 // app config
 const app = express();
@@ -16,8 +17,8 @@ app.use(express.json());
 app.use(cors());
 
 // api routes
-
 app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
