bde20e689ab0c584af21e10227032438acf3d472
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-02-16 19:32:10 +0530

    create add product controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,17 +1,74 @@
-// add product logic
+import { v2 as cloudinary } from "cloudinary";
+import Product from "../models/productModel.js";
 
-const addProduct = async () => {};
+// add product logic
+const addProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      image,
+      category,
+      subCategory,
+      sizes,
+      bestseller,
+      date,
+    } = req.body;
+
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
+
+    let imagesUri = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
+
+    const productData = {
+      name,
+      description,
+      price: Number(price),
+      category,
+      subCategory,
+      sizes: JSON.parse(sizes),
+      bestseller: bestseller === "true" ? true : false,
+      image: imagesUri,
+      date: Date.now(),
+    };
+
+    const productInfo = await Product.create(productData);
+
+    return res.status(200).json({
+      success: true,
+      message: "product added successfully...",
+      productInfo,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // list prodcuts
-
 const listProduct = async () => {};
 
 // remove product
-
 const removeProduct = async () => {};
 
 // info for single product
-
 const singleProduct = async () => {};
 
 export { addProduct, listProduct, removeProduct, singleProduct };
