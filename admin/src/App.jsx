278f5bc3be0c6ada231851426d30fee7d7f350f7
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-02-17 22:57:36 +0530

    create login page with functinality

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import AddProduct from "./pages/AddProduct";
@@ -6,25 +6,35 @@ import ListProducts from "./pages/ListProducts";
 import Orders from "./pages/Orders";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import { ToastContainer } from "react-toastify";
+
+export const backendURI = import.meta.env.VITE_BACKEND_URI;
 
 const App = () => {
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
 
   return (
     <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
       {token === "" ? (
-        <Login />
+        <Login setToken={setToken} />
       ) : (
         <>
-          <Navbar />
+          <Navbar setToken={setToken} />
           <hr />
           <div className="flex w-full">
             <Sidebar />
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-500 text-base">
               <Routes>
-                <Route path="/add" element={<AddProduct />} />
-                <Route path="/list" element={<ListProducts />} />
-                <Route path="/orders" element={<Orders />} />
+                <Route path="/add" element={<AddProduct token={token} />} />
+                <Route path="/list" element={<ListProducts token={token} />} />
+                <Route path="/orders" element={<Orders token={token} />} />
               </Routes>
             </div>
           </div>
--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import { useState } from "react";
+import { backendURI } from "../App";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
-const Login = () => {
+const Login = ({ setToken }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      const res = await axios.post(backendURI + "/api/user/admin", {
+        email,
+        password,
+      });
+
+      if (res?.data?.success) {
+        setToken(res.data.token);
+      } else {
+        toast.error(res?.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.response?.data?.message);
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center w-full">
       <div className="bg-white shadow-md rounded-lg px-8 py-6 max-w-md">
         <h1 className="text-2xl font-bold mb-4">Admin Panel</h1>
 
-        <form>
+        <form onSubmit={onSubmitHandler}>
           <div className="mb-3 min-w-72">
             <p className="text-sm font-medium text-gray-700 mb-2">
               Email Address
@@ -15,17 +42,34 @@ const Login = () => {
               className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
               type="email"
               placeholder="your@email.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
-          <div className="mb-3 min-w-72">
+          <div className="mb-3 min-w-72 relative">
             <p className="text-sm font-medium text-gray-700 mb-2">Password</p>
-            <input
-              className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
-              type="password"
-              placeholder="Enter Your Password"
-              required
-            />
+            <div className="relative">
+              <input
+                className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none pr-10"
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter Your Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-3 flex items-center"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <AiOutlineEyeInvisible size={20} />
+                ) : (
+                  <AiOutlineEye size={20} />
+                )}
+              </button>
+            </div>
           </div>
 
           <button
